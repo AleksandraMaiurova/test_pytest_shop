2235fa3804853cf4790a999287a4f58c92d3d472
AleksandraMaiurova <asmaiurova@gmail.com>
2023-10-15 13:00:09 +0300

    first commit

---
+++ models.py
@@ -0,0 +1,61 @@
+class Product:
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def __init__(self, name, price, description, quantity):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.quantity = quantity
+
+    def check_quantity(self, quantity) -> bool:
+        return self.quantity >= quantity
+
+    def buy(self, quantity):
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError('продуктов не хватает')
+
+    def __hash__(self):
+        return hash(self.name + self.description)
+
+class Cart:
+    products: dict[Product, int]
+
+    def __init__(self):
+        self.products = {}
+
+    def add_product(self, product: Product, buy_count=1):
+        if product not in self.products:
+            self.products[product] = buy_count
+        else:
+            self.products[product] += buy_count
+
+
+    def remove_product(self, product: Product, remove_count=None):
+        if product in self.products:
+            if not remove_count:
+                del self.products[product]
+            elif remove_count >= self.products[product]:
+                del self.products[product]
+            else:
+                self.products[product] -= remove_count
+
+    def clear(self):
+        self.products.clear()
+
+    def get_total_price(self) -> float:
+        total_price = 0
+        for product, quantity in self.products.items():
+            total_price += product.price * quantity
+        return total_price
+
+    def buy(self):
+        for product, quantity in self.products.items():
+            product.quantity -= quantity
+            if product.quantity < 0:
+                raise ValueError
+        self.clear()
---
+++ test_shop.py
@@ -0,0 +1,98 @@
+import pytest
+from test_shop.models import Product, Cart
+
+
+@pytest.fixture
+def product():
+    return Product("book", 100, "This is a book", 1000)
+
+@pytest.fixture
+def cart():
+    return Cart()
+
+
+class TestProducts:
+    def test_product_check_quantity(self, product):
+        assert product.check_quantity(1000)
+        assert product.check_quantity(999)
+        assert product.check_quantity(0)
+        assert product.check_quantity(-1)
+        assert product.check_quantity(1001) == False
+
+
+    def test_product_buy(self, product):
+        product.buy(1)
+        assert product.check_quantity(999)
+        product.buy(999)
+        assert product.check_quantity(0)
+
+    def test_product_buy_more_than_available(self, product):
+          with pytest.raises(ValueError, match=r"продуктов не хватает"):
+            product.buy(9999)
+
+
+class TestCart:
+    def test_add_product(self, product, cart):
+        cart.add_product(product, 1)
+        assert product in cart.products
+        assert cart.products[product] == 1
+        cart.add_product(product, 5)
+        assert cart.products[product] == 6
+
+    def test_remove_product(self, product, cart):
+        cart.add_product(product, 5)
+        cart.remove_product(product, 4)
+        assert cart.products[product] == 1
+        cart.add_product(product, 5)
+        cart.remove_product(product)
+        assert product not in cart.products
+        cart.add_product(product, 5)
+        cart.remove_product(product, 5)
+        assert product not in cart.products
+        cart.add_product(product, 5)
+        cart.remove_product(product, 20)
+        assert product not in cart.products
+
+    def test_clear(self, product, cart):
+        cart.clear()
+        assert product not in cart.products
+        cart.add_product(product, 5)
+        cart.clear()
+        assert product not in cart.products
+
+
+    def test_get_total_price(self, product, cart):
+        cart.add_product(product, 5)
+        assert cart.get_total_price() == product.price*5
+        cart.clear()
+        cart.add_product(product)
+        assert cart.get_total_price() == product.price
+
+
+    def test_buy(self, product, cart):
+        cart.add_product(product, 5)
+        cart.buy()
+        assert product not in cart.products
+        assert product.quantity == 995
+        cart.add_product(product, 995)
+        cart.buy()
+        assert product not in cart.products
+        assert product.quantity == 0
+        with pytest.raises(ValueError):
+            cart.add_product(product)
+            cart.buy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
